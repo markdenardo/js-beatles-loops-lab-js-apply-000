d03094e694a6679bb2bd6d186bf45dbeebfff37d
Mark DeNardo <mark.denardo@gmail.com>
2019-07-30 21:24:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
   return theBeatlesPlay;
  }
  
- function johnLennonFacts(johnLennonArray){
+ function johnLennonFacts(ArrayBuffer){
     var i = 0;
     while (johnLennonArray[i]<johnLennonFacts.length){
       text += johnLennonFacts[i]+"!!!";
