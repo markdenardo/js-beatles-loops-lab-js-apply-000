1871625440b0fe43c066ed73499549869d55cfe3
Mark DeNardo <mark.denardo@gmail.com>
2019-07-30 21:31:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,10 +9,6 @@
  }
  
  function johnLennonFacts(factsArray){
-    var factsArray = ["He was the last Beatle to learn to drive",
-        "He was never a vegetarian",
-        "He was a choir boy and boy scout",
-        "He hated the sound of his own voice"];
     while (factsArray[i]<factsArray.length){
       text += factsArray[i]+"!!!";
       i++;
