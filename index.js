a4898d983cbe6c36ab77b06e07a1073631287c9a
Mark DeNardo <mark.denardo@gmail.com>
2019-07-30 20:56:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+ function theBeatlesPlay(musicians, instruments){
+  var theBeatlesPlay = [];
+  
+   for (var i = 0; i < musicians.length; i++){
+    musicians[i]+" plays "+ instruments[i];
+     theBeatlesPlay.push(musicians[i]+" plays "+ instruments[i]);
+   }
+  return theBeatlesPlay;
+ }
+ 
+ function johnLennonFacts(johnLennonFacts){
+    var i = 0;
+    while (johnLennonFacts[i]<johnLennonFacts.length){
+      text += johnLennonFacts[i]+"!!!";
+      i++;
+    }
+ }
+ 
+ function iLoveTheBeatles(iLoveTheBeatles){
+   var emptyArray = [];
+   do {
+     
+   }
+   while (i<15);
+   
+ }
+ 
